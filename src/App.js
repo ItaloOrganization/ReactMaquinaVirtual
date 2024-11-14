3a68474b41b2d4cf88ff0d0ee445f963ea31cbfe
gem install rails <gem install rails>
2024-11-14 00:41:34 -0400

    N Commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacttttt
+          Learn Reacttttt NASHE
         </a>
       </header>
     </div>
